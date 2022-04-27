cb808d5a8725cdfa22d7744586aa87c8c625ecfb
hernanmejia <hmejiabenitez@gmail.com>
2022-04-27 18:29:35 -0500

    Api

---
+++ src/test/java/com/example/ApiElfren/ApiElfrenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiElfren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiElfrenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
